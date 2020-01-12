c59fc82cd1f3697605856fbdf8d6e5087f62e05d
KC <egbosikelechi@gmail.com>
2020-01-12 16:23:19 +0100

    first commit

---
+++ stmp.py
@@ -0,0 +1,56 @@
+import logging
+import os
+from queue import Queue
+from threading import Thread
+from time import time
+from decouple import config
+
+from download import setup_download_dir, get_links, download_link
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+
+class DownloadWorker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            directory, link = self.queue.get()
+            try:
+                download_link(directory, link)
+            finally:
+                self.queue.task_done()
+
+
+def main():
+    ts = time()
+    client_id = config('client_id')
+    if not client_id:
+        raise Exception("Couldn't find client_id environment variable!")
+    download_dir = setup_download_dir()
+    links = get_links(client_id)
+    # Create a queue to communicate with the worker threads
+    queue = Queue()
+    # Create 6 worker threads
+    for x in range(6):
+        worker = DownloadWorker(queue)
+        # Setting daemon to True will let the main thread exit even though the workers are blocking
+        worker.daemon = True
+        worker.start()
+    # Put the tasks into the queue as a tuple
+    for link in links:
+        logger.info('Queueing {}'.format(link))
+        queue.put((download_dir, link))
+    # Causes the main thread to wait for the queue to finish processing all the tasks
+    queue.join()
+    logging.info('Took %s', time() - ts)
+
+if __name__ == '__main__':
+    main()
